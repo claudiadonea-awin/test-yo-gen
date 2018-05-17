597dfe1d6123c5ea683164b7d2ab83fe2beb03ab
Claudia Donea <claudia.donea@awin.com>
2018-05-17 14:41:52 +0100

    initial commit

---
+++ src/sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-component',
+  template: `<h1>Sample component from npm public package</h1>`
+})
+export class SampleComponent {
+
+  constructor() {
+  }
+
+}
